b0f8b6901a2023fb7b91b04e4fecb38dcca560f3
LesterGallagher <LesterGallagher@gmail.com>
2019-04-20 17:31:01 +0200

    prod

--- src/sharing-modal.js
+++ src/sharing-modal.js
@@ -1,32 +1,27 @@
+require('./css/sharing-buttons.css');
+const createSharingButtons = require('./mustache-templates/sharing-buttons.mustache').default;
 const $ = require('jquery');
 const conf = require('./conf');
 
-const generateSharingButtonsHTML = require('./sharing-buttons');
-
 const init = () => {
-    const html = generateSharingButtonsHTML({
-        facebook: true,
-        twitter: true,
-        google: true,
-        tumblr: true,
-        email: true,
-        pinterest: true,
-        linkedin: true,
-        reddit: true,
-        xing: true,
-        whatsapp: true,
-        hackernews: true,
-        vk: true,
-        telegram: true,
-    },
-        $('#mainh').val() + ' Mindmap',
-        document.querySelector('meta[name=description]').getAttribute('content'),
-        'https://mindmapper.esstudio.site',
-        location.pathname + location.search,
-        'https://mindmapper.esstudio.site/public/img/mindmap.png',
-        'medium');
+    var size = 'medium';
+    size = size.toLowerCase();
+    if (size !== 'small' && size !== 'large') size = 'medium';
 
-    $('#sharing-modal .modal-body').html(html);
+    var description = encodeURIComponent(document.querySelector('meta[name=description]').getAttribute('content'));
+    var title = encodeURIComponent($('#mainh').val() + ' Mindmap');
+    var url = encodeURIComponent(new window.URL(location.pathname + location.search, 'https://mindmapper.esstudio.site').href);
+    var imageUrl = 'https://mindmapper.esstudio.site/public/img/mindmap.png';
+
+    $('#sharing-modal .modal-body').append('Share using this url: <strong>' + decodeURIComponent(url) + '</strong>');
+
+    $('#sharing-modal .modal-body').append(createSharingButtons({
+        title,
+        description,
+        url,
+        imageUrl,
+        size
+    }));
 
 }
 
